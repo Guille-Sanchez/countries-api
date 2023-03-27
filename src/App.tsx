e231f3063a8b35ece1943b77f09287c81224404c
Guillermo <guillesafran@gmail.com>
2023-03-27 11:48:24 -0400

    Add basic structure to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
 import './App.css'
+import countires from './mockups/data.json'
 
 function App (): JSX.Element {
+  const regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'] as const
   return (
     <div className="App">
+      <header>
+        <h1>Where in the world?</h1>
+        <div>
+          <button>moon</button>
+          <p>Dark Mode</p>
+        </div>
+      </header>
+      <main>
+        <div>
+          <button>Lupa</button>
+          <input type="text" />
+        </div>
 
+        <select name="filter-by-region">
+          <option value=''>Filter by region</option>
+          {
+            regions.map((region) => {
+              return (<option value={region} key={region}>{region}</option>)
+            })
+          }
+        </select>
+
+        <ul>
+          {
+            countires.map((country) => {
+              return (
+                <li key={country.name}>
+                  <img src={country.flags.png} alt={country.name} />
+                  <div>
+                    <p>{country.name}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region}</p>
+                    <p>Capital: {country.capital}</p>
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
     </div>
   )
 }
